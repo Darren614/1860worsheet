f90bcac25f50b33af22b55ec4d76376dbe394b99
Darren614 <dxtk5177@leeds.ac.uk>
2025-05-02 11:24:08 +0000

    main

--- Xor.asm
+++ Xor.asm
@@ -1,10 +1,14 @@
+// compute z=x xor y, same as (x or y) and not(x and y)
+// not modified the value initially stored in R0 and R1
+
 @R0
-D=M
+D=M // D=x
 @R1
-D=D&M
-D=!D
+D=D&M // D=x or y
+D=!D // D=not(x or y)
+//stored value at here temporarily
 @temp1
-M=D
+M=D 
 
 @R0
 D=M
